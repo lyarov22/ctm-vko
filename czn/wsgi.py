7fed3072cc08cc0d0b135d7f3170d43bf2f12351
Фёдоров Илья <96327924+lyarov22@users.noreply.github.com>
2024-01-20 19:47:57 +0600

    Update wsgi.py

--- czn/wsgi.py
+++ czn/wsgi.py
@@ -2,8 +2,15 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
+from czn import settings
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'czn.settings')
 
 application = get_wsgi_application()
 application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles'))
 application.add_files(os.path.join(os.path.dirname(__file__), 'media'), prefix='media/')  # Если у вас есть медиа файлы
+
+# Enable insecure serving of static files when DEBUG is True
+if settings.DEBUG:
+    from django.contrib.staticfiles.handlers import StaticFilesHandler
+    application = StaticFilesHandler(application)
